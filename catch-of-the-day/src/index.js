29240dd81d6a5907a9e2871e16f87d8c823c6e05
Arafat M <github@arafatm.com>
2019-05-27 01:52:35 +0200

    Our first React Component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,3 +1,10 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
 
-alert('is this working?')
+class StorePicker extends React.Component {
+  render() {
+    return <p>Hello!</p>
+  }
+}
+ 
+render(<p>heyyyy</p>, document.querySelector('#main'));
