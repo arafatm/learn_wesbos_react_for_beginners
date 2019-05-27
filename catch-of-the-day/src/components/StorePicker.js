26ddb98e7b91884629b728e16c4c3769b16ebb0c
Arafat M <github@arafatm.com>
2019-05-27 02:24:30 +0200

    Can only return a single element in JSX

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,6 +3,7 @@ import React from 'react';
 class StorePicker extends React.Component {
   render() {
     return (
+      <p>React will yell at me for returning multiple elements</p>
       <form className="store-selector">
         <h2>Please Enter a Store</h2>
       </form>
