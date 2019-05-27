1b2667e364dcf326fd3edf690ca55894d2039e25
Arafat M <github@arafatm.com>
2019-05-27 02:30:51 +0200

    comments in JSX are weird. Have to put them in `{ ../.. }`

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,6 +4,7 @@ class StorePicker extends React.Component {
   render() {
     return (
         <form className="store-selector">
+          { /* Have to comment in curlys */ }
           <h2>Please Enter a Store</h2>
         </form>
     )
