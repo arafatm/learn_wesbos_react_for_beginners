2b7c44143d521d5a9e3be1bcd3dd98dc0eec1346
Arafat M <github@arafatm.com>
2019-05-27 02:34:40 +0200

    A working StorePicker component

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,8 +4,9 @@ class StorePicker extends React.Component {
   render() {
     return (
         <form className="store-selector">
-          { /* Have to comment in curlys */ }
           <h2>Please Enter a Store</h2>
+          <input type="text" required placeholder="Store Name" />
+          <button type="submit">Visit Store -></button>
         </form>
     )
   }
