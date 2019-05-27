fa914108e953708004845685fcdb47b8a5ad24af
Arafat M <github@arafatm.com>
2019-05-27 02:29:11 +0200

    readme

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,12 +3,9 @@ import React from 'react';
 class StorePicker extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <p>Fragments allow returning sibling elements</p>
         <form className="store-selector">
           <h2>Please Enter a Store</h2>
         </form>
-      </React.Fragment>
     )
   }
 }
