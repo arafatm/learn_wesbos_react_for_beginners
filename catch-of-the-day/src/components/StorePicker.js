207f12f904359b437d8abc8d354b58b4982b7004
Arafat M <github@arafatm.com>
2019-05-27 02:15:57 +0200

    - can't use  on a Form. Must use

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return React.createElement('p', { classname: 'hey'}, 'Heyooo');
+    return <form class="store-selector"></form>
   }
 }
 
