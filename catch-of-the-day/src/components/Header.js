b68cac98a0f8f2bb7547e8ceaa9940439cd1040b
Arafat M <github@arafatm.com>
2019-05-27 03:16:16 +0200

    Initial Header component

---
+++ catch-of-the-day/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+    <header className="top">
+      <h1>Catch of the Day</h1>
+      <h3 className="tagline">
+        Fresh Seafood Market
+      </h3>
+    </header>
+    )
+  }
+}
+export default Header;
